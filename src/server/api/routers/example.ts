c8b94b6a4269791d277823aefe56ef33bbb9f826
Othman Shareef <othmanosx@gmail.com>
2023-06-06 02:51:20 +0300

    Feat: create trpc endpoints

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -18,6 +18,67 @@ export const exampleRouter = createTRPCRouter({
     return ctx.prisma.example.findMany();
   }),
 
+  getPlan: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.plan.findFirst({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+  editPlan: protectedProcedure
+    .input(
+      z.object({
+        budget: z.string(),
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.plan.upsert({
+        create: {
+          budget: input.budget,
+          userId: ctx.session.user.id,
+        },
+        update: {
+          budget: input.budget,
+        },
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+  getExpenses: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.expenses.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+  addExpense: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        cost: z.string(),
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.expenses.create({
+        data: {
+          name: input.name,
+          cost: input.cost,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+
+  deleteExpense: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.expenses.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
